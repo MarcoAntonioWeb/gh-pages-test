0bd7a18a30aac8d4da139b0eba05e4add60d8c09
Marco <programacao.curso2016@gmail.com>
2020-06-10 15:22:35 -0300

    Updates

---
+++ precache-manifest.f0b05026e8a2c64acc40ccf80e263158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea7815c8df69b02703094033958bc2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad581e5bcfe70103249b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3c107d204ae176a1ec5",
+    "url": "/static/js/2.d1d1a012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1d1a012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad581e5bcfe70103249b",
+    "url": "/static/js/main.f9fcb0b6.chunk.js"
+  },
+  {
+    "revision": "5f76aa23708438f56c8d",
+    "url": "/static/js/runtime-main.19745b08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
