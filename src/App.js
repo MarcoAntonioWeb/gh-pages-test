f315710a9c9d283b6a81420dc4b5c8f1fc5283ce
Marco <programacao.curso2016@gmail.com>
2020-06-10 17:04:51 -0300

    atualiza

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Rafaella o Teste deu certo!</h2>
+        <h2>Rafaella o Teste deu Certo!</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
